46694924d9934e1f821be0a1ef104103a9ebc84e
elle7777 <iustindesrobitu@gmail.com>
2024-09-22 08:07:27 +0100

    a

---
+++ Exercises/Basic Programs/C++ Program find the size of int. float double and char/main.cpp
@@ -0,0 +1,16 @@
+// Size of
+// version 0.1.0
+// @Iustin Desrobitu
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "the size of int is " << sizeof(int) << " bytes" << endl;
+    cout << "the size of long is " << sizeof(long) << " bytes" << endl;
+    cout << "the size of float is " << sizeof(float) << " bytes" << endl;
+    cout << "the size of double is " << sizeof(double) << " bytes" << endl;
+    cout << "the size of char is " << sizeof(char) << " bytes" << endl;
+    cout << "the size of bool is " << sizeof(bool) << " bytes" << endl;
+}
---
+++ Exercises/Basic Programs/C++ Program to multiply two floating point numbers/main.cpp
@@ -0,0 +1,17 @@
+// Floating multiplication
+// verison 0.1.0
+// @Iustin Desrobitu
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    double number_1;
+    double number_2;
+
+    cout << "This progeam multiplies two numbers" << endl;
+    cout << "Insert the first number: "; cin >> number_1;
+    cout << "insert the second number: "; cin >> number_2;
+    cout << "The result is " << number_1 * number_2 << endl;
+}
