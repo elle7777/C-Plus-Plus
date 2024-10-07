264b4643b9a4bc9bf5a5fd12a23d4a75ee1b77de
elle7777 <iustindesrobitu@gmail.com>
2024-10-07 17:56:26 +0100

    a

---
+++ Exercises/Basic Programs/C++ Program to find simple interest/main.cpp
@@ -0,0 +1,29 @@
+// Simple Interest 
+// version 0.1.0
+// @Iustin Desrobitu
+
+#include <iostream>
+
+using namespace std;
+double interestCalculator(double initialAmount, double interestRate, int interestTime)
+{
+    double finalAmount = (initialAmount * interestRate * interestTime) / 100;
+    return finalAmount;
+}
+int main()
+{
+    double initialAmount; 
+    double interestRate; //percentage
+    int interestTime; //years
+
+    cout << "This program finds simple interest" << endl;
+    cout << "Insert the initial Amount: ";
+    cin >> initialAmount;
+    cout << "insert the years of interest: ";
+    cin >> interestTime;
+    cout << "insert the interest rate: ";
+    cin >> interestRate;
+    
+    double finalAmount = interestCalculator(initialAmount, interestRate, interestTime);
+    cout << "Your amount after " << interestTime << " is " << finalAmount << "$" << endl;
+}
